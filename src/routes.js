3b5863a299d8629981c2c9ff11258fac56a50705
Nithish <nithishsk03@gmail.com>
2025-01-07 11:57:18 +0530

    first commit

---
+++ src/routes.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Icon } from '@chakra-ui/react';
+import {
+  MdBarChart,
+  MdPerson,
+  MdHome,
+  MdLock,
+  MdOutlineShoppingCart,
+} from 'react-icons/md';
+
+// Admin Imports
+import MainDashboard from 'views/admin/default';
+import NFTMarketplace from 'views/admin/marketplace';
+import Profile from 'views/admin/profile';
+import DataTables from 'views/admin/dataTables';
+import RTL from 'views/admin/rtl';
+
+// Auth Imports
+import SignInCentered from 'views/auth/signIn';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const routes = [
+  {
+    name: 'Main Dashboard',
+    layout: '/admin',
+    path: '/default',
+    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
+    component: <MainDashboard />,
+  },
+  {
+    name: 'NFT Marketplace',
+    layout: '/admin',
+    path: '/nft-marketplace',
+    icon: (
+      <Icon
+        as={MdOutlineShoppingCart}
+        width="20px"
+        height="20px"
+        color="inherit"
+      />
+    ),
+    component: <NFTMarketplace />,
+    secondary: true,
+  },
+  {
+    name: 'Data Tables',
+    layout: '/admin',
+    icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
+    path: '/data-tables',
+    component: <DataTables />,
+  },
+  {
+    name: 'Profile',
+    layout: '/admin',
+    path: '/profile',
+    icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
+    component: <Profile />,
+  },
+  {
+    name: 'Sign In',
+    layout: '/auth',
+    path: '/sign-in',
+    icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
+    component: <SignInCentered />,
+  },
+  {
+    name: 'RTL Admin',
+    layout: '/rtl',
+    path: '/rtl-default',
+    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
+    component: <RTL />,
+  },
+];
+
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_startTransition: true, // Enable the future flag for transition behavior in React Router v7
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Use the RouterProvider to pass the configured router
+root.render(
+  <RouterProvider router={router} />
+);
+
+export default routes;
